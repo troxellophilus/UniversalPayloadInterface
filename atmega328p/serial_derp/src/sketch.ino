4449a4b01d13b19a9601afea76df86044d33aadc
Drew Troxell <dtroxell@calpoly.edu>
2015-04-20 17:51:42 -0700

    Initial commit w/ atmega328p code for MAVLink.

---
+++ atmega328p/serial_derp/src/sketch.ino
@@ -0,0 +1,43 @@
+/*
+ * Veryify Connection
+ *   Verify the connection between the atmega328p & the raspberry pi via uart handshake.
+ *
+ * Drew Troxell
+ */ 
+
+#define BUFFER_SIZE 4
+
+uint8_t buf[BUFFER_SIZE];
+uint16_t siz = 0;
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial);
+
+    pinMode(D0, INPUT);
+
+    while (Serial.available() <= 0) {
+        Serial.println("HEYHEYHEY");
+        delay(300);
+    }
+}
+
+
+void loop() {
+    uint16_t nBA = Serial.available();
+
+    while (nBA-- > 0) {
+        buf[siz++] = Serial.read();
+        if (size == BUFFER_SIZE) {
+            receivedWord(*(uint32_t *)buf);
+            size = 0;
+        }
+    }
+
+    delay(100);
+}
+
+void receivedWord(uint32_t w) {
+    // echo
+    Serial.write(&w, BUFFER_SIZE);
+}
