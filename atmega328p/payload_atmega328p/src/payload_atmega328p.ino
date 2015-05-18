3b4b471a4c750c2eff0c14f558d7209205c20dbb
Drew Troxell <dtroxell@calpoly.edu>
2015-05-17 21:22:33 -0700

    Current state, no correct reception from apm yet.

--- atmega328p/payload_atmega328p/src/payload_atmega328p.ino
+++ atmega328p/payload_atmega328p/src/payload_atmega328p.ino
@@ -1,26 +1,35 @@
 // Arduino MAVLink test code.
 
+//#include <FastSerial.h>
 #include <mavlink.h>        // Mavlink interface
 
 #define PAYLOAD_SYSTEM_ID 101 // System ID for MAVlink packets
 
+int ledPin = 25;
+
+//FastSerialPort0(Serial); 
+
 void setup() {
-	Serial.begin(57600);
+	pinMode(ledPin, OUTPUT);
+	Serial.begin(115200);
 }
 
 void loop() { 
 	// Define the system type (see mavlink_types.h for list of possible types) 
-	int system_type = MAV_QUADROTOR;
+	int system_type = MAV_TYPE_ONBOARD_CONTROLLER;
 	int autopilot_type = MAV_AUTOPILOT_ARDUPILOTMEGA;
-	int component_id = MAV_COMP_ID_PAYLOAD;
+	int component_id = 101; //MAV_COMP_ID_PAYLOAD;
+	int base_mode = MAV_MODE_FLAG_TEST_ENABLED;
+	int sys_status = MAV_STATE_STANDBY;
 	
 	// Initialize the required buffers 
 	mavlink_message_t msg; 
 	uint8_t buf[MAVLINK_MAX_PACKET_LEN];
 	
 	// Setup the heartbeat message
-	mavlink_msg_heartbeat_pack(PAYLOAD_SYSTEM_ID, component_id, &msg, system_type, autopilot_type);
-	
+	//mavlink_msg_heartbeat_pack(PAYLOAD_SYSTEM_ID, component_id, &msg, system_type, autopilot_type, base_mode, 0, sys_status);
+	mavlink_msg_mission_request_list_pack(PAYLOAD_SYSTEM_ID, component_id, &msg, MAV_TYPE_QUADROTOR, MAV_COMP_ID_ALL);
+
 	// Copy the message to send buffer 
 	uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
 	
@@ -28,12 +37,13 @@ void loop() {
 	Serial.write(buf, len);
 	
 	// slow down there young padawan
-	delay(500);
+	delay(1000);
 	
 	comm_receive();
 }
 
 void comm_receive() { 
+	static int w = 0;
 	mavlink_message_t msg; 
 	mavlink_status_t status;
 	
@@ -45,13 +55,19 @@ void comm_receive() {
 		if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) { 
 			// Handle message
  			switch(msg.msgid) {
-			        case MAVLINK_MSG_ID_SET_MODE: {
-			        	// set mode
-			        }
-			        break;
-			        case MAVLINK_MSG_ID_ACTION:
-					// EXECUTE ACTION
-				break;
+			        case MAVLINK_MSG_ID_HEARTBEAT:
+					if (w == 0) {
+						digitalWrite(ledPin, HIGH);
+						w = 1;
+					}
+					else {
+						digitalWrite(ledPin, LOW);
+						w = 0;
+					}
+			        	break;
+				case MAVLINK_MSG_ID_MISSION_ITEM:
+					digitalWrite(ledPin, HIGH);
+					break;
 				default:
 					//Do nothing
 				break;
